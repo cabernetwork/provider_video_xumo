965aac46feb3880398a3403553aa92405b921ab6
rocky4546 <24759693+rocky4546@users.noreply.github.com>
2023-12-04 11:31:22 -0600

    Changed HTTP retries to not hard coded

--- provider_video_xumo/lib/channels.py
+++ provider_video_xumo/lib/channels.py
@@ -67,11 +67,11 @@ class Channels(PluginChannels):
                 if ch_db_data[0]['json']['thumbnail'] == thumbnail:
                     thumbnail_size = ch_db_data[0]['json']['thumbnail_size']
                 else:
-                    thumbnail_size = self.get_thumbnail_size(thumbnail, ch_id)
+                    thumbnail_size = self.get_thumbnail_size(thumbnail, 2, ch_id)
             else:
                 enabled = True
                 hd = 0
-                thumbnail_size = self.get_thumbnail_size(thumbnail, ch_id)
+                thumbnail_size = self.get_thumbnail_size(thumbnail, 2, ch_id)
 
             ch_callsign = channel_dict['callsign']
             vod = False
@@ -166,4 +166,4 @@ class Channels(PluginChannels):
         if self.config_obj.data[self.config_section]['player-stream_type'] == 'm3u8redirect':
             return stream_url
 
-        return self.get_best_stream(stream_url, _channel_id)
+        return self.get_best_stream(stream_url, 2, _channel_id)
